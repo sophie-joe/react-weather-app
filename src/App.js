4c652984717ccc4f43fae03d38cba06a3ba5a342
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-20 17:52:04 +1200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sophie Joe and is{" "}
+        <a
+          href="https://github.com/sophie-joe/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
