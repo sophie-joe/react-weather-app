c83d25d8e730d5013fb860e519cf0c3de7df9744
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-21 15:02:14 +1200

    Adding bootstrap and updates

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sophie Joe and is{" "}
-        <a
-          href="https://github.com/sophie-joe/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">hello</button>
+        <footer>
+          This project was coded by Sophie Joe and is{" "}
+          <a
+            href="https://github.com/sophie-joe/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
