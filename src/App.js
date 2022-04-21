954414e5578da0275b1bfdb9da1969dd5303c6ca
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-21 16:01:33 +1200

    HTML base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <button className="btn btn-primary">hello</button>
+        <Weather />
         <footer>
           This project was coded by Sophie Joe and is{" "}
           <a
