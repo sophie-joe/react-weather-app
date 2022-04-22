844e4f8acc3543b0a8bd64cb591d1cdec4706c35
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-04-22 12:01:26 +1200

    CSS updates

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,11 +30,15 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="sunny with cloud"
-          ></img>
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="sunny with cloud"
+              className="float-left"
+            />{" "}
+            <span className="current-temperature">6</span>
+            <span className="current-unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
